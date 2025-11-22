445d3d4d9411d03bacf80069857e2ec95e1603ef
zaidachhwa <zaidachhwa8486@gmail.com>
2025-11-22 16:00:25 +0530

    backend and frontend code segregated

--- backend/middleware/error.middleware.js
+++
@@ -1,6 +0,0 @@
-export const errorHandler = (err, req, res, next) => {
-  console.error(err);
-  res
-    .status(err.status || 500)
-    .json({ message: err.message || "Internal Server Error" });
-};
