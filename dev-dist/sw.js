15890cc7f0b1e2407eaa99cc39292cc34b8d39f5
karan <karankanakarajan@example.com>
2026-08-20 10:29:55 +0530

    feat: implement unified login components for admin, student, and teacher roles

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-877eb569'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "/index.html",
-    "revision": "0.0j2r3cmjcjk"
+    "revision": "0.d63c2din72o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
