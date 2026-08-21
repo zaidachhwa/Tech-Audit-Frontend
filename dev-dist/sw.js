a6d793735eb1a6b0a0039ac30fe10748e773772f
karan <karankanakarajan@example.com>
2026-08-21 15:22:23 +0530

    feat: add service worker configuration for offline caching and network routing via Workbox

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-877eb569'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "/index.html",
-    "revision": "0.7at6dqb3iso"
+    "revision": "0.m4aic3720ag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
